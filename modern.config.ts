34bebc4df1418e01c24ac6404fa68676771dd6f1
xudong li <accounts_629032965330d45819a6e546@mail.teambition.com>
2022-07-12 09:29:42 +0800

    feat: 集成windicss

--- modern.config.ts
+++ modern.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig } from '@modern-js/app-tools';
+import WindiCSSWebpackPlugin from 'windicss-webpack-plugin';
 
 // https://modernjs.dev/docs/apis/config/overview
-export default defineConfig({});
+export default defineConfig({
+  tools: {
+    webpack: (config, { appendPlugins }) => {
+      // 添加插件
+      appendPlugins([new WindiCSSWebpackPlugin() as any]);
+    },
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,9 @@
-import './index.css';
+import 'windi.css';
 
 const Index = () => (
   <div className="container-box">
-    <main>
-      <div className="logo">
-        <img
-          src="https://lf3-static.bytednsdoc.com/obj/eden-cn/ylaelkeh7nuhfnuhf/modernjs-cover.png"
-          width="300"
-          alt="Modern.js Logo"
-        />
-      </div>
-      <p className="description">
-        Get started by editing <code className="code">src/Index.tsx</code>
-      </p>
-      <div className="grid">
-        <a href="https://modernjs.dev/docs/start" className="card">
-          <h2>Quick Start</h2>
-        </a>
-        <a href="https://modernjs.dev/docs/guides" className="card">
-          <h2>Handbook</h2>
-        </a>
-        <a href="https://modernjs.dev/docs/apis" className="card">
-          <h2>API Reference </h2>
-        </a>
-        <a
-          href="https://modernjs.dev/coming-soon"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="card">
-          <h2>Community </h2>
-        </a>
-      </div>
-    </main>
-    <footer className="footer">
-      <a
-        href="https://modernjs.dev"
-        target="_blank"
-        rel="noopener noreferrer">
-        Powered by Modern.js
-      </a>
-    </footer>
+    <div className="w-200px h-200px bg-red-200"></div>
   </div>
-
 );
 
 export default Index;
